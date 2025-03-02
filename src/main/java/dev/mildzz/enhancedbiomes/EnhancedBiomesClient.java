1ada96fb79fa2044f5c0c7a69cd479083cb34820
Mildzz <swaimisaiah@hotmail.com>
2025-03-02 10:04:07 -0600

    Initial commit

---
+++ src/main/java/dev/mildzz/enhancedbiomes/EnhancedBiomesClient.java
@@ -0,0 +1,10 @@
+package dev.mildzz.enhancedbiomes;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EnhancedBiomesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/dev/mildzz/enhancedbiomes/EnhancedBiomesDataGenerator.java
@@ -0,0 +1,11 @@
+package dev.mildzz.enhancedbiomes;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class EnhancedBiomesDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
